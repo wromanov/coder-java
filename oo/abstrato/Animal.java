187bc74568f01c42808594dc6959dd26639ed6f0
Walace <walacecastro@live.com>
2023-08-08 14:06:32 -0300

    Autalização, aula classe abstrata.

--- oo/abstrato/Animal.java
+++ oo/abstrato/Animal.java
@@ -1,5 +1,21 @@
 package oo.abstrato;
 
-public class Animal {
+public abstract class Animal {
+
+    /* Usamos a classe abstrata quando temos uma classe tão genérica, que não conseguimos finalizar
+    o algoritmo de um método, assim deixando a implementação para quem herdar esse método por herança.*/
+
+    //Uma classe abstrata não pode ser estanciada, apenas estendida para outra classe por meio de herança.
+
+    //Uma classe abstrata pode conter métodos concretos e abstratos, é como se fosse uma classe inacabada.
+
+    public String respirar() {
+        return "Respirando Oxigênio.";
+    }
+
+    //Só é possível ter método abstrato em classe abstrata
+
+
+    public abstract String mover(); //Método abstrato não tem corpo.
 
 }
