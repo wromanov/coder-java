bd0ee70fc2f3abcdab23d30546b22b9eca2c1c17
Walace <walacecastro@live.com>
2023-08-08 14:24:24 -0300

    Atualização, aula classe abstrata.

--- oo/heranca/Desafio/Esportivo.java
+++ oo/heranca/Desafio/Esportivo.java
@@ -2,12 +2,18 @@ package oo.heranca.Desafio;
 
 public interface Esportivo {
 
-    /* Por padrão todo método dentro de uma interface é abstrato (não tem corpo) e seu
+    /* Por padrão todos os métodos dentro de uma interface são abstratos (não tem corpo) e seu
     *  nível de visibilidade é público.*/
-
 
     void ligarTurbo();
     void desligarTurbo();
+
+    /* Método default deixa a critério usar o valor padrão do método ou sobrescrevê-lo,
+    como se fosse um método opcional dentro de uma interface, já que os métodos que não são
+    default precisam ser implementados. */
+    default int velocidadeAr() {
+        return 1;
+    }
 
 
 
