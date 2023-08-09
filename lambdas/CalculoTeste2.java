9a92733c9daa63cdb1e56f27cb21f7139f2a7b02
Walace <walacecastro@live.com>
2023-08-09 14:26:12 -0300

    atualização aula lambda.

--- lambdas/CalculoTeste2.java
+++ lambdas/CalculoTeste2.java
@@ -4,9 +4,13 @@ public class CalculoTeste2 {
 
     public static void main(String[] args) {
 
+        /* Lambdas são funções anonimas que são possíveis de associar os resultados dessas funções a uma variável.*/
+
+        //Função Lambda diretamente associada a variável calc.
         Calculo calc = (x, y) -> {
-            return x + y;
+            return  x + y;
         };
+
         System.out.println(calc.executar(2, 3));
 
         //Quando omitimos a chave na função lambda, ela irá automaticamente realizar um return da função.
@@ -15,7 +19,7 @@ public class CalculoTeste2 {
         calc = (x, y) -> x * y;
         System.out.println(calc.executar(2, 3));
 
-        Calculo cal02 = (x, y) -> x - y;
+        Calculo cal02 = (x, y) -> { double resultado; resultado =  x - y; return resultado;};
         System.out.println(cal02.executar(2,3));
     }
 
