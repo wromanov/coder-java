81474221b3cb4bccbdaa6411562ef0b594a47d63
Walace <walacecastro@live.com>
2023-08-09 13:54:46 -0300

    atualização aula lambda.

--- lambdas/Calculo.java
+++ lambdas/Calculo.java
@@ -1,6 +1,15 @@
 package lambdas;
 
+@FunctionalInterface
 public interface Calculo {
     double executar(double a, double b);
+
+
+//Essa anotação "@FunctionalInterface" valida que trata-se de uma interface funcional e que pode ser implementado apenas um método.
+//A partir das interfaces funcionais que serão criadas as funções do tipo lambdas (funções anonimas usadas dentro do código)
+//https://docs.oracle.com/javase/8/docs/api/java/util/function/package-summary.html
+
+
+
 
 }
---
+++ lambdas/CalculoTeste2.java
@@ -0,0 +1,23 @@
+package lambdas;
+
+public class CalculoTeste2 {
+
+    public static void main(String[] args) {
+
+        Calculo calc = (x, y) -> {
+            return x + y;
+        };
+        System.out.println(calc.executar(2, 3));
+
+        //Quando omitimos a chave na função lambda, ela irá automaticamente realizar um return da função.
+        //Porém não podemos escrever blocos de códigos dentro da função.
+
+        calc = (x, y) -> x * y;
+        System.out.println(calc.executar(2, 3));
+
+        Calculo cal02 = (x, y) -> x - y;
+        System.out.println(cal02.executar(2,3));
+    }
+
+
+}
