b0561161094d62285124c46a28433bd1395e99c6
Walace <walacecastro@live.com>
2023-08-10 15:04:52 -0300

    Foarch Lambda

--- lambdas/ForEach.java
+++ lambdas/ForEach.java
@@ -2,22 +2,32 @@ package lambdas;
 
 import java.util.Arrays;
 import java.util.List;
+
 public class ForEach {
+
     public static void main(String[] args) {
 
         List<String> aprovados = Arrays.asList("Walace", "Eliane", "Sophia", "Nicolly", "Lucas", "Ezequiel");
 
         for (String nomes : aprovados) {
             System.out.println(nomes);
-
         }
 
         System.out.println("\nLambda #01...");
-
         aprovados.forEach(nome -> System.out.println(nome + "!!!"));
 
         System.out.println("\nMethod Reference...");
         aprovados.forEach(System.out::println);
+
+
+        System.out.println("\nLambda #02...");
+        aprovados.forEach(nome -> meuImprimir(nome));
+
+
+    }
+
+    static void meuImprimir(String nome) {
+        System.out.println(nome);
     }
 
 
