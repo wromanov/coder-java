f055b36c9d833e0e71267e08bd86ca7d3398ba5e
walacedelgado <walacecastro@live.com>
2023-08-20 14:17:26 -0300

    Aula Supplier.

---
+++ lambdas/Fornecedor.java
@@ -0,0 +1,17 @@
+package lambdas;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Fornecedor {
+    public static void main(String[] args) {
+
+        Supplier<List<String>> umaLista = () -> Arrays.asList("Ana", "Bia", "Edu");
+
+        System.out.println(umaLista.get());
+
+
+    }
+}
