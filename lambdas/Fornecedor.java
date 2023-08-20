99e08abfb2bd0c12739474852b848e3537e5f06d
walacedelgado <walacecastro@live.com>
2023-08-20 14:22:30 -0300

    Aula Supplier.

--- lambdas/Fornecedor.java
+++ lambdas/Fornecedor.java
@@ -8,6 +8,8 @@ import java.util.function.Supplier;
 public class Fornecedor {
     public static void main(String[] args) {
 
+        //Supplier é o tipo de interface que não recebe um parâmetro e retorna alguma coisa.
+
         Supplier<List<String>> umaLista = () -> Arrays.asList("Ana", "Bia", "Edu");
 
         System.out.println(umaLista.get());
