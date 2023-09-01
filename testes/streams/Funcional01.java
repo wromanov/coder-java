214216284abdb3fbe6f74209d896ecec6a5488eb
walacedelgado <walacecastro@live.com>
2023-09-01 14:24:12 -0300

    Lambdas Desafio.

--- testes/streams/Funcional01.java
+++ testes/streams/Funcional01.java
@@ -1,5 +1,7 @@
 package testes.streams;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.BinaryOperator;
 
 public class Funcional01 {
@@ -9,5 +11,13 @@ public class Funcional01 {
         BinaryOperator<Double> teste01 = (a, b) -> {return a + b;};
         System.out.println(teste01.apply(1.2, 2.1));
 
+        List<String> aprovados = Arrays.asList("Walace", "Eliane", "Sophia");
+        aprovados.forEach(nome -> meuImprimir(nome));
+
     }
+    static void meuImprimir(String nome) {
+        System.out.println(nome + "Seja bem vindo!!!");
+    }
+
+
 }
