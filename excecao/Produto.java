d8f392eae128d9268338f2c7425b2049eacab499
Walace <walacecastro@live.com>
2023-09-12 17:10:51 -0300

    Aula - Tratamento de Exceções.

---
+++ excecao/Produto.java
@@ -0,0 +1,86 @@
+package excecao;
+
+import java.util.Objects;
+
+public class Produto {
+
+    private String nome;
+    private int quantidadeEstoque;
+    private boolean ativo;
+
+    public Produto(String nome, int quantidadeEstoque) {
+        setNome(nome);
+        adicionarEstoque(quantidadeEstoque);
+    }
+
+    public void ativar(){
+        this.ativo = true;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        //Lança exceção se o nome for nulo
+        Objects.requireNonNull(nome, "Nome deve ser informado.");
+        this.nome = nome;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void adicionarEstoque(int quantidade) {
+        if (quantidade == 0){
+            throw new IllegalArgumentException("A quantidade deve ser maior que 0.");
+        }
+        this.quantidadeEstoque += quantidade;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public boolean isInativo() {
+        return !isAtivo();
+    }
+
+    //Proteger o método com um lançamento de exceção para não receber valores negativos.
+    //Assim interrompendo a execução desse método, para não deixar a instrução ser executada.
+    public void retirarEstoque(int quantidade) {
+        if (quantidade < 0) {
+            //Lançando Exceção
+            throw new IllegalArgumentException("Quantidade não pode ser negativa para retirada no estoque.");
+
+            //Lançando Exceção modo 2
+            //IllegalArgumentException excecao = new IllegalArgumentException("Quantidade não pode ser negativa para retirada no estoque.");
+            //throw excecao;
+
+            //Utilizando exceção pré-definida, exceções são objetos em java.
+        }
+
+        if (isInativo()) {
+            throw new IllegalStateException("Não pode realizar retirada de um produto inativo do estoque.");
+        }
+
+        if (this.quantidadeEstoque - quantidade < 0) {
+            throw new IllegalArgumentException("Quantidade a retirar do estoque, não pode ser maior que o volume contido no estoque.");
+        }
+
+        this.quantidadeEstoque -= quantidade;
+    }
+
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public String toString() {
+        return "Nome: " + getNome()
+                + "\nEstoque: "
+                + getQuantidadeEstoque()
+                + "\nStatus do Produto: " + isAtivo();
+    }
+
+}
---
+++ excecao/ProdutoTeste.java
@@ -0,0 +1,31 @@
+package excecao;
+
+public class ProdutoTeste {
+    public static void main(String[] args) {
+
+        /* Proteger o construtor com lançamento de exceção para:
+         * Não receber um nome Null
+         * Não receber quantidade menor ou igual 0
+         */
+        Produto p1 = new Produto("Caneta", 50);
+
+        p1.adicionarEstoque(10);
+        p1.ativar();
+
+        /* Proteger o método com um lançamento de exceção para:
+         * Não receber valores negativos.
+         * Não retirar produtos com status inativo.
+         * Não retirar quantidades maiores do que existe no estoque.
+         * Assim interrompendo a execução desse método, para não deixar a instrução ser executada */
+
+        p1.retirarEstoque(30);
+
+        System.out.println(p1);
+
+
+
+
+
+
+    }
+}
